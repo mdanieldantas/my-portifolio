f5b481ed3b390eeda6873627ddcd106bb9da398d
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-08-27 16:35:04 -0300

    feat: sobre

--- src/assets/pages/Home/Home.tsx
+++ src/assets/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import NaviBar from "../../components/NavBar/NavBar";
+import About from "./sections/About/About";
 import Hero from "./sections/Hero/Hero";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <NaviBar />
       <Hero />
+      <About />
     </>
   );
 };
--- src/assets/pages/Home/sections/About/About.tsx
+++ src/assets/pages/Home/sections/About/About.tsx
@@ -1,5 +1,56 @@
+import { Container, Grid, styled, Typography, Box } from "@mui/material";
+
 const About = () => {
-  return <>About</>;
+  const StyledAbout = styled("div")(({ theme }) => ({
+    background: "#ffffff",
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    [theme.breakpoints.up("xs")]: {
+      paddingTop: "100px",
+    },
+    [theme.breakpoints.up("md")]: {
+      paddingTop: "0",
+    },
+  }));
+
+  const StyledText = styled(Typography)(({ theme }) => ({
+    color: "#000000",
+  }));
+
+  return (
+    <StyledAbout>
+      <Container maxWidth="lg">
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <StyledText variant="h2" textAlign="center" pb={2}>
+              Sobre Mim
+            </StyledText>
+            <StyledText variant="body1" textAlign="center" pb={2}>
+              Estudante de Análise e Desenvolvimento de Sistemas, com formação
+              complementar em Psicologia. Possuo uma base sólida em
+              desenvolvimento web e um perfil versátil, capaz de se adaptar a
+              diferentes desafios. Minhas habilidades incluem programação
+              (JavaScript, React, Node.JS), versionamento de código (GitHub) e
+              gestão de bancos de dados. A experiência em Psicologia me
+              proporcionou um olhar humanizado para a tecnologia, permitindo
+              desenvolver soluções que atendam às necessidades dos usuários.
+              Busco um estágio para aplicar meus conhecimentos e desenvolver
+              minhas habilidades em um ambiente dinâmico e desafiador.
+            </StyledText>
+            <Box
+              sx={{
+                width: "100%",
+                height: "2px",
+                backgroundColor: "#000000",
+                marginTop: "16px",
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Container>
+    </StyledAbout>
+  );
 };
 
 export default About;

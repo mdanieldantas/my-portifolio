ab83233236656de8622c2eecd2b60607c8135cb6
M Daniel  Dantas <contatomarcosdgomes@gmail.com>
2024-08-27 10:55:16 -0300

    feat : hero, styledButton, AnimatedBackgroud

--- src/assets/pages/Home/sections/Hero/Hero.tsx
+++ src/assets/pages/Home/sections/Hero/Hero.tsx
@@ -1,24 +1,29 @@
 // Importa componentes e funções do Material-UI
-import { Button, Container, Grid, styled, Typography } from "@mui/material";
+import { Box, Container, Grid, styled, Typography } from "@mui/material";
 // Importa um icone de download do Material-UI
 import DownloadIcon from "@mui/icons-material/Download";
 // Importa um icone de email do Material-UI
 import EmailIcon from "@mui/icons-material/Email";
 // Importa uma imagem de avatar
 import Avatar from "../../../../images/avatarDanielSemtop.jpg";
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 // Define o componente Hero como uma função
 const Hero = () => {
   // Cria um componente estilizado chamado StyledHero, que é uma div com fundo preto
-  const StyledHero = styled("div")(() => ({
-    background: "black",
+  const StyledHero = styled("div")(({ theme }) => ({
+    background: theme.palette.primary.main,
     height: "100vh",
+    display: "flex",
+    alignItems: "center",
   }));
 
   // Cria um componente estilizado chamado StyledImg, que é uma imagem com largura de 30% e bordas arredondadas
-  const StyledImg = styled("img")(() => ({
-    width: "100%",
+  const StyledImg = styled("img")(({ theme }) => ({
+    width: "80%",
     borderRadius: "50%",
+    border: `1px solid ${theme.palette.primary.contrastText}`,
   }));
 
   // Retorna o JSX que será renderizado
@@ -27,18 +32,38 @@ const Hero = () => {
       <StyledHero>
         <Container maxWidth="lg">
           <Grid container spacing={2}>
-            <Grid item xs={12} md={4}>
-              <StyledImg src={Avatar} />
+            <Grid item xs={12} md={5}>
+              <Box position="relative">
+                <Box position="absolute" width={"150%"} top={-100} right={0}>
+                  <AnimatedBackground />
+                </Box>
+                <Box position="absolute" textAlign={"center"}>
+                  <StyledImg src={Avatar} />
+                </Box>
+              </Box>
             </Grid>
-            <Grid item xs={12} md={8}>
-              <Typography color="primary" variant="h1" textAlign="center">
+            <Grid item xs={12} md={7}>
+              <Typography
+                color="primary.contrastText"
+                variant="h1"
+                textAlign="center"
+              >
                 M Daniel Dantas
               </Typography>
-              <Typography color="primary" variant="h2" textAlign="center">
+              <Typography
+                color="primary.contrastText"
+                variant="h2"
+                textAlign="center"
+              >
                 Programador Web
               </Typography>
 
-              <Grid container display={"flex"} justifyContent={"center"}>
+              <Grid
+                container
+                display={"flex"}
+                justifyContent={"center"}
+                spacing={3}
+              >
                 <Grid
                   item
                   xs={12}
@@ -46,10 +71,10 @@ const Hero = () => {
                   display="flex"
                   justifyContent="center"
                 >
-                  <Button>
+                  <StyledButton>
                     <DownloadIcon />
-                    Download CV
-                  </Button>
+                    <Typography>Download CV</Typography>
+                  </StyledButton>
                 </Grid>
 
                 <Grid
@@ -59,10 +84,10 @@ const Hero = () => {
                   display="flex"
                   justifyContent="center"
                 >
-                  <Button>
+                  <StyledButton>
                     <EmailIcon />
-                    Fale comigo
-                  </Button>
+                    <Typography>Fale comigo</Typography>
+                  </StyledButton>
                 </Grid>
               </Grid>
             </Grid>
